a44031d2af934ab20694cc72e1727fddf1552f45
Your Name <michaelndula@gmail.com>
2024-10-10 19:37:52 +0300

    Adding Backend service

---
+++ src/main/java/com/emerson_care/emerson_care/EmersonCareApplication.java
@@ -0,0 +1,13 @@
+package com.emerson_care.emerson_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmersonCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmersonCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerson_care/emerson_care/EmersonCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson_care.emerson_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmersonCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
