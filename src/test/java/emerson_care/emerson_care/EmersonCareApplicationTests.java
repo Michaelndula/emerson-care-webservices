6fd5d1220facf63aa1f0cc340c88f4af15e439a3
Your Name <michaelndula@gmail.com>
2025-01-02 23:46:10 +0300

    Create gradle.yml

---
+++ src/test/java/emerson_care/emerson_care/EmersonCareApplicationTests.java
@@ -0,0 +1,13 @@
+package emerson_care.emerson_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmersonCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
